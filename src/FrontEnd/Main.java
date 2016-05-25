1377591c2f32d34eb17c8dabbc8aa06c5b8f9972
fabio984 <fabiojfl984@hotmail.com>
2016-05-25 17:06:27 +0100

    Revert "ricardo edita"

--- src/FrontEnd/Main.java
+++ src/FrontEnd/Main.java
@@ -21,7 +21,7 @@ public class Main {
     //login.setVisible(true);
     //AccessLawsuit login = new AccessLawsuit(system);
     //login.setVisible(true);
-    Entity login = new Entity(system);
+    AccessLawsuit login = new AccessLawsuit(system);
     login.setVisible(true);
     }
     
